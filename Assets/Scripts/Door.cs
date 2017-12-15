5893711061a139e6faba503bc29a8bc7be30cd83
Jibran Khan <jibrankhan00@hotmail.com>
2017-12-15 17:46:03 -0500

    More comments
    More comments

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -20,6 +20,9 @@ public class Door : MonoBehaviour {
 		
 	}
 
+	/// <summary>
+	/// Opens the door.
+	/// </summary>
 	public void OpenDoor(){
 		//Flame.SetActive (false);
 		//EyeMask.SetActive (true);
@@ -28,12 +31,18 @@ public class Door : MonoBehaviour {
 			_GM.GetComponent<GameManager> ().DoorOpened ();
 	}
 
+	/// <summary>
+	/// Doors the opened.
+	/// </summary>
 	public void DoorOpened(){
 		Flame.SetActive (false);
 		EyeMask.SetActive (true);
 		DoorParticle.SetActive(true);
 	}
 
+	/// <summary>
+	/// Closes the door.
+	/// </summary>
 	public void CloseDoor(){
 		//Flame.SetActive (true);
 		//EyeMask.SetActive (false);
