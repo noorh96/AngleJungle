1c1bc8717c2582a64d13e72afe0290a291e74e21
Jibran Khan <jibrankhan00@hotmail.com>
2018-01-04 11:53:24 -0800

    Cleanup
    Moved around sprites into correct folder from materials
    Deleted old stage select scene
    Deleted level testing scenes
    Fixed scaling issue in treasure room

--- Assets/Scripts/Main/MusicManager.cs
+++ Assets/Scripts/Main/MusicManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MusicManager : MonoBehaviour {
+	
 	public AudioSource BGM_as;
 	public AudioSource MAP_as;
 	public AudioSource CABIN_as;
@@ -17,120 +18,150 @@ public class MusicManager : MonoBehaviour {
 	public AudioSource CreakClick_as;
 	private Coroutine curCo;
 	private AudioSource curAs;
-	// Use this for initialization
-	void Awake () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-		
 
-	public void PauseBGM(){
+	public void PauseBGM()
+	{
 		//BGM_as.Pause();
 	}
 
-	public void StopBGM(){
+	public void StopBGM()
+	{
 		curCo = StartCoroutine(fadeOut(BGM_as));
 	}
 
-	public void PlayBGM(){
+	public void PlayBGM()
+	{
 		if (curAs == BGM_as)
 			return;
+		
 		curCo = StartCoroutine(fadeOut(curAs));
 		curAs = BGM_as;
 		curAs.volume = 1f;
 		curAs.Play();
 	}
-	public void PlayMAPSound(){
+
+	public void PlayMAPSound()
+	{
 		if (curAs == MAP_as)
 			return;
+		
 		curCo = StartCoroutine(fadeOut(curAs));
 		curAs = MAP_as;
 		curAs.volume = 1f;
 		curAs.Play();
 	}
-	public void PlayCabinSound(){
+
+	public void PlayCabinSound()
+	{
 		if (curAs == CABIN_as)
 			return;
+		
 		curCo = StartCoroutine(fadeOut(curAs));
 		curAs = CABIN_as;
 		curAs.volume = 1f;
 		curAs.Play();
 	}
-	public void PlayCabin2Sound(){
+
+	public void PlayCabin2Sound()
+	{
 		if (curAs == CABIN_as2)
 			return;
+		
 		curCo = StartCoroutine(fadeOut(curAs));
 		curAs = CABIN_as2;
 		curAs.volume = 1f;
 		curAs.Play();
 	}
-	public void PlayS1Sound(){
+
+	public void PlayS1Sound()
+	{
 		if (curAs == S1_as)
 			return;
+		
 		curCo = StartCoroutine(fadeOut(curAs));
 		curAs = S1_as;
 		curAs.volume = 1f;
 		curAs.Play();
 	}
-	public void PlayS2Sound(){
+
+	public void PlayS2Sound()
+	{
 		if (curAs == S2_as)
 			return;
-		if (curCo != null) {
+		
+		if (curCo != null) 
+		{
 			StopCoroutine (curCo);
 		}
+
 		curCo = StartCoroutine(fadeOut(curAs));
 		curAs = S2_as;
 		curAs.volume = 1f;
 		curAs.Play();
 	}
-	public void PlayS3Sound(){
+
+	public void PlayS3Sound()
+	{
 		if (curAs == S3_as)
 			return;
-		if (curCo != null) {
+		
+		if (curCo != null) 
+		{
 			StopCoroutine (curCo);
 		}
+
 		curCo = StartCoroutine(fadeOut(curAs));
 		curAs = S3_as;
 		curAs.volume = 1f;
 		curAs.Play();
 	}
-	public void PlayS4Sound(){
+
+	public void PlayS4Sound()
+	{
 		if (curAs == S4_as)
 			return;
-		if (curCo != null) {
+		
+		if (curCo != null) 
+		{
 			StopCoroutine (curCo);
 		}
+
 		curCo = StartCoroutine(fadeOut(curAs));
 		curAs = S4_as;
 		curAs.volume = 1f;
 		curAs.Play();
 	}
-	public void PlayS5Sound(){
+
+	public void PlayS5Sound()
+	{
 		if (curAs == S5_as)
 			return;
-		if (curCo != null) {
+		
+		if (curCo != null) 
+		{
 			StopCoroutine (curCo);
 		}
+
 		curCo = StartCoroutine(fadeOut(curAs));
 		curAs = S5_as;
 		curAs.volume = 1f;
 		curAs.Play();
 	}
 
-	public void PlayMenuButton(){
+	public void PlayMenuButton()
+	{
 		MenuClick_as.Stop ();
 		MenuClick_as.Play ();
 	}
 		
-	public void PlayClickButton(){
+	public void PlayClickButton()
+	{
 		Click_as.Stop ();
 		Click_as.Play ();
 	}
 
-	public void PlayCreakClickButton(){
+	public void PlayCreakClickButton()
+	{
 		CreakClick_as.Stop ();
 		CreakClick_as.Play ();
 	}
@@ -148,17 +179,22 @@ public class MusicManager : MonoBehaviour {
 
 	IEnumerator fadeOut(AudioSource auSource)
 	{
-		if (auSource != null) {
+		if (auSource != null) 
+		{
 			float t = 1f;
-			while (t > 0.0f) {
+
+			while (t > 0.0f) 
+			{
 				t -= Time.deltaTime * 0.8f;
 				auSource.volume = t;
 				yield return new WaitForSeconds (0);
 				if (curAs == auSource)
 					yield break;
 			}
+
 			if (curAs == auSource)
 				yield break;
+			
 			auSource.volume = 0.0f;
 			auSource.Stop ();
 		}
